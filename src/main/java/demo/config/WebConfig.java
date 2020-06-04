6e0d0e47cf092c8d660269f8e92cff8788ce091d
sukhwa <3023abc@easymedia.net>
2020-06-04 09:18:03 +0900

    test

--- src/main/java/demo/config/WebConfig.java
+++ src/main/java/demo/config/WebConfig.java
@@ -39,7 +39,7 @@ public class WebConfig implements WebMvcConfigurer  {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/user/**")
-                .addResourceLocations("file:/D:/DEMO/uploadfiles/user");
+                //.addResourceLocations("file:/D:/DEMO/uploadfiles/user");
                 //.addResourceLocations("file:/DATA/video/"); //리눅스 root에서 시작하는 폴더 경로
     }
 
